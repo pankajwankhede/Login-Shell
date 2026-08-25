c8d9374cf06f8128f3be2b934997092fdc75d485
Pankaj wankhede <pankajwankhede91@gmail.com>
2026-08-25 13:28:28 -0400

    Update Test.tsx

--- Test.tsx
+++ Test.tsx
@@ -1,9 +1,9 @@
+
 location = /ssoshell {
     return 301 /ssoshell/;
 }
 
 location ^~ /ssoshell/ {
-
     proxy_set_header X-Forwarded-Host $host;
     proxy_set_header X-Forwarded-Proto $scheme;
     proxy_set_header X-Real-IP $remote_addr;
@@ -11,6 +11,18 @@ location ^~ /ssoshell/ {
     include /appbin/sf/nginx/servers/common/PCF_Host.conf;
 
     set $proxy_pass_url https://shell-sso-ui.app.dev1.use1.pcf.syfbank.com;
+
+    proxy_pass $proxy_pass_url;
+}
+
+location ^~ /ssoauth/ {
+    proxy_set_header X-Forwarded-Host $host;
+    proxy_set_header X-Forwarded-Proto $scheme;
+    proxy_set_header X-Real-IP $remote_addr;
+
+    include /appbin/sf/nginx/servers/common/PCF_Host.conf;
+
+    set $proxy_pass_url https://shell-sso-service.app.dev1.use1.pcf.syfbank.com;
 
     proxy_pass $proxy_pass_url;
 }
